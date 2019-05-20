9fdc2420f17e6b8c085a2867746b3b72c23cda7f
weiwei717 <34641650+weiwei717@users.noreply.github.com>
2019-05-20 20:37:44 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -2,9 +2,16 @@
 // role that the `App` class played in HW3.
 //
 // See HW4 writeup for more hints and details.
-class App {
-  constructor() {
+class App
+{
+  constructor()
+  {
     // TODO(you): Implement the constructor and add fields as necessary.
+    const menu = document.querySelector("#menu");
+    this.menuScreen = new MenuScreen(menu);
+
+    const music = document.querySelector("#music");
+    this.musicScreen = new MusicScreen(music);
   }
   // TODO(you): Add methods as necessary.
 }
--- play-button.js
+++ play-button.js
@@ -2,9 +2,16 @@
 // it toggles audio playback.
 //
 // See HW4 writeup for more hints and details.
-class PlayButton {
-  constructor() {
+class PlayButton
+{
+  constructor(containerElement)
+  {
     // TODO(you): Implement the constructor and add fields as necessary.
+    this.containerElement = containerElement;
+    containerElement.addEventListener('click', () => {
+      containerElement.classList.toggle('pause');
+      document.dispatchEvent(new CustomEvent("status"));
+    });
   }
   // TODO(you): Add methods as necessary.
 }
